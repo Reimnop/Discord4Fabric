cacbcc864df696be7c1f0f04a0437d76c032cded
Reimnop <56994757+Reimnop@users.noreply.github.com>
2022-06-24 23:23:27 +0700

    Added refresh cache command

--- src/main/java/me/reimnop/d4f/commands/ModCommands.java
+++ src/main/java/me/reimnop/d4f/commands/ModCommands.java
@@ -1,6 +1,7 @@
 package me.reimnop.d4f.commands;
 
 import me.reimnop.d4f.Discord4Fabric;
+import me.reimnop.d4f.exceptions.GuildException;
 import me.reimnop.d4f.utils.Utils;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.server.command.CommandManager;
@@ -15,6 +16,22 @@ public final class ModCommands {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             dispatcher.register(
                     CommandManager.literal("discord4fabric")
+                            .then(CommandManager.literal("refresh_cache")
+                                    .requires(source -> source.hasPermissionLevel(4))
+                                    .executes(context -> {
+                                        try {
+                                            context.getSource().sendFeedback(
+                                                    Text.literal("Refreshing cache!"),
+                                                    false
+                                            );
+                                            Discord4Fabric.DISCORD.initCache();
+                                            return 1;
+                                        } catch (GuildException e) {
+                                            context.getSource().sendError(Text.literal("An unexpected error occurred! Check logs for more details"));
+                                            Utils.logException(e);
+                                            return 0;
+                                        }
+                                    }))
                             .then(CommandManager.literal("reload")
                                     .requires(source -> source.hasPermissionLevel(4))
                                     .executes(context -> {
