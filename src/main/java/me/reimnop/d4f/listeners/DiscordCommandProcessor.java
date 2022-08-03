869354723abc6c223be02ae2469cdfdc0f4cf0ac
Reimnop <rayngu2005@gmail.com>
2022-08-04 02:36:39 +0700

    Implement #31

---
+++ src/main/java/me/reimnop/d4f/listeners/DiscordCommandProcessor.java
@@ -0,0 +1,69 @@
+package me.reimnop.d4f.listeners;
+
+import me.reimnop.d4f.Config;
+import me.reimnop.d4f.Discord;
+import me.reimnop.d4f.Discord4Fabric;
+import me.reimnop.d4f.events.DiscordMessageReceivedCallback;
+import me.reimnop.d4f.utils.Utils;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.server.MinecraftServer;
+
+import java.util.Map;
+
+public final class DiscordCommandProcessor {
+    private DiscordCommandProcessor() {}
+
+    private interface DiscordCommandHandler {
+        void handle(MinecraftServer server);
+    }
+
+    private static final Map<String, DiscordCommandHandler> commandHandlers = Map.of(
+            "ping", server -> Discord4Fabric.DISCORD.sendPlainMessage("Pong! :ping_pong:"),
+            "tps", server -> Discord4Fabric.DISCORD.sendPlainMessage("Server TPS: " + Utils.getTpsAsString()),
+            "playerlist", server -> {
+                int maxPlayers = server.getMaxPlayerCount();
+                String[] playerNames = server.getPlayerNames();
+
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder
+                        .append(playerNames.length)
+                        .append("/")
+                        .append(maxPlayers)
+                        .append(" players currently online:\n");
+
+                int playersToShow = Math.min(playerNames.length, 5);
+                for (int i = 0; i < playersToShow; i++) {
+                    stringBuilder
+                            .append(playerNames[i])
+                            .append(playersToShow - i == 1 ? "" : ", ");
+                }
+                if (playerNames.length > playersToShow) {
+                    stringBuilder
+                            .append(" *and ")
+                            .append(playerNames.length - playersToShow)
+                            .append(" more*");
+                }
+                Discord4Fabric.DISCORD.sendPlainMessage(stringBuilder.toString());
+            }
+    );
+
+    private static final String prefix = "!";
+
+    public static void init(Config config) {
+        DiscordMessageReceivedCallback.EVENT.register((user, message) -> {
+            if (message.getChannel().getIdLong() != config.channelId) {
+                return;
+            }
+
+            String content = message.getContentRaw();
+            if (!content.startsWith(prefix)) {
+                return;
+            }
+
+            String cmd = content.substring(prefix.length());
+            if (commandHandlers.containsKey(cmd)) {
+                commandHandlers.get(cmd).handle((MinecraftServer) FabricLoader.getInstance().getGameInstance());
+            }
+        });
+    }
+}
--- src/main/java/me/reimnop/d4f/utils/Utils.java
+++ src/main/java/me/reimnop/d4f/utils/Utils.java
@@ -2,13 +2,17 @@ package me.reimnop.d4f.utils;
 
 import eu.pb4.placeholders.api.PlaceholderHandler;
 import eu.pb4.placeholders.api.Placeholders;
+import me.lucko.spark.api.Spark;
+import me.lucko.spark.api.SparkProvider;
+import me.lucko.spark.api.statistic.StatisticWindow;
+import me.lucko.spark.api.statistic.types.DoubleStatistic;
 import me.reimnop.d4f.Discord4Fabric;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.Identifier;
 
-import java.util.Arrays;
 import java.util.Map;
 import java.util.UUID;
 
@@ -45,6 +49,33 @@ public final class Utils {
 
     public static String getNameCachePath() {
         return FabricLoader.getInstance().getConfigDir() + "/d4f_name_cache.json";
+    }
+
+    public static String getTpsAsString() {
+        if (FabricLoader.getInstance().isModLoaded("spark")) {
+            Spark spark = SparkProvider.get();
+            DoubleStatistic<StatisticWindow.TicksPerSecond> tps = spark.tps();
+
+            if (tps == null) {
+                return String.valueOf(roundTps(getShittyTps((MinecraftServer) FabricLoader.getInstance().getGameInstance())));
+            }
+
+            double tpsLast10Secs = tps.poll(StatisticWindow.TicksPerSecond.SECONDS_10);
+            double tpsLast5Mins = tps.poll(StatisticWindow.TicksPerSecond.MINUTES_5);
+            double tpsLast15Mins = tps.poll(StatisticWindow.TicksPerSecond.MINUTES_15);
+
+            return roundTps(tpsLast10Secs) + " / " + roundTps(tpsLast5Mins) + " / " + roundTps(tpsLast15Mins) + " (10 secs / 5 mins / 15 mins)";
+        }
+        return String.valueOf(roundTps(getShittyTps((MinecraftServer) FabricLoader.getInstance().getGameInstance())));
+    }
+
+    private static double roundTps(double tps) {
+        return Math.floor(tps * 10.0) / 10.0;
+    }
+
+    // very appropriate function name
+    private static double getShittyTps(MinecraftServer server) {
+        return Math.min(1000.0 / server.getTickTime(), 20.0);
     }
 
     public static String getNicknameFromUser(User user) {
